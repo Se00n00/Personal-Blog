82dafdda8a0e6dd3c29659383b67a772a01a623c
Mohit kumar <Bluesky5april2021@gmail.com>
2025-01-09 11:31:25 +0530

    minnor changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
+    fontFamily:{
+      heading: [ "Roboto", "serif" ],
+      content: [ "Open Sans", "serif" ]
+    },
     extend: {
       colors: {
         primary: 'rgb(var(--color-primary) / <alpha-value>)',
