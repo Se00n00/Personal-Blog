916348c8d52a46367a2626423bb8777aec9cedbd
Mohit kumar <Bluesky5april2021@gmail.com>
2025-01-09 15:15:52 +0530

    gynamic article formatting

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHighlightOptions } from 'ngx-highlightjs';
+
+
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimationsAsync(),
+    provideHighlightOptions({
+      fullLibraryLoader: () => import('highlight.js')
+    }),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: Aura
+            }
+    })
+  ]
 };
--- frontend/src/app/articles/articles.component.ts
+++ frontend/src/app/articles/articles.component.ts
@@ -1,18 +1,36 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+
+import { ButtonModule } from 'primeng/button'
+import { FieldsetModule } from 'primeng/fieldset';
 
 @Component({
   selector: 'app-articles',
-  imports: [MatIconModule, CommonModule,],
+  imports: [MatIconModule, CommonModule, ButtonModule,FieldsetModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.css'
 })
+
 export class ArticlesComponent {
+
   type = "web Design"
   numView = 100
   numComments = 1
   numLiked = 10
+  conditioned:string =""
+
+  quoteText = "This iS a quote"
+  ImageTitle = "Just A Title"
+  imageSrc = "https://c4.wallpaperflare.com/wallpaper/292/446/516/ultra-wide-photography-wallpaper-preview.jpg"
+  text = "Sample Text"
+  headingText = "Heading"
+  subHeadingText = "subHeading"
+  listItem = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, ea?"
+  tagName = "Deep Learning"
+  items = ["a","a","a","a","a","a","a","a","a","a","a","a","a"]
+
+  codeLanguage="typescript"
+  code = "#include stdio.h int main() printf()"
 }
